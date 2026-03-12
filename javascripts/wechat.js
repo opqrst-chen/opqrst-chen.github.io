a3384e9873fd7a1f7cc58700b2b1da335a56b49b
opqrst-chen <jiaweichen.china@gmail.com>
2026-03-12 14:30:39 +0800

    update

---
+++ javascripts/wechat.js
@@ -0,0 +1,9 @@
+// 显示微信二维码
+function showWechatQr() {
+    document.getElementById('wechatOverlay').style.display = 'block';
+}
+
+// 隐藏微信二维码
+function closeWechatQr() {
+    document.getElementById('wechatOverlay').style.display = 'none';
+}
